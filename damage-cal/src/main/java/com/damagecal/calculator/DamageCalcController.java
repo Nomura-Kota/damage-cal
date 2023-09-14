d6102f83266949e7d5286ceaf5f6d039b8caeee1
Nomura-Kota <nomura.kota2005@gmail.com>
2023-09-14 14:42:00 +0900

    バックアップ2

--- damage-cal/src/main/java/com/damagecal/calculator/DamageCalcController.java
+++ damage-cal/src/main/java/com/damagecal/calculator/DamageCalcController.java
@@ -1,24 +1,11 @@
 package com.damagecal.calculator;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "http://damage-cal.com")
 public class DamageCalcController {
-
-    @PostMapping("/calculate")
-    public ResponseEntity<CalculationResult> calculateDamage(@RequestBody DamageRequest request) {
-        int result = calculateDamage(request.getHealth(), request.getAttack());
-        CalculationResult calculationResult = new CalculationResult(result);
-        return ResponseEntity.ok(calculationResult);
-    }
-    
-    private int calculateDamage(int health, int attack) {
-        return health - attack;
-    }
+	
 }
 
